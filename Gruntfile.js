d95e544dc449190269c72102735c340729881123
kito99 <kito.mann@virtua.com>
2016-09-17 21:39:10 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+		exec: {
+		  readme: 'asciidoctor "README.asc"',            
+		  session: 'asciidoctor -T asciidoctor-reveal.js/templates/slim "angular-def.primeng-jsweet-jaxrs.asc"',
+            pdf: 'asciidoctor-pdf *.asc'		  
+		},
+        'connect': {
+            demo: {
+                options: {
+                    open: true,
+                    keepalive: true,
+                    port: 9091,
+					base: "."	
+                }
+            }
+        }
+    });
+
+	grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('server', ['connect']);
+    
+	grunt.registerTask('build-readme', ['exec:readme']);	
+	grunt.registerTask('build-session', ['exec:session']);	
+	grunt.registerTask('build-pdf', ['exec:pdf']);		
+        
+	grunt.registerTask('default', ['build-readme', 'build-session', 'build-pdf']);	
+    
+};
